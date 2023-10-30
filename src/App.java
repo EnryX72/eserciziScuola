7eb853c32289afcefd6c3261aa37076e26162192
EnryX72 <negrettoenrico@gmail.com>
2023-10-30 18:32:40 +0100

    aiut-

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner tastiera = new Scanner(System.in);
+
+    static int nStudenti = 5;
     public static void main(String[] args) {
-        
+        String studenti[] = new String[nStudenti];
+        int voti[] = new int[nStudenti];
+
+        caricaStudenti(studenti);
+    }
+
+    public static void caricaStudenti(String[] studenti){
+        for(int i = 0; i < nStudenti; i++){
+            System.out.println("Inserisci il nome dello studnete " + (i+1) + ": ");
+            studenti[i] = tastiera.nextLine();
+        }
     }
 }
