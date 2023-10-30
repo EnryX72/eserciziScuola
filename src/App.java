d0e5ff43bab27d4ac1868f24c28625ea5a69340d
EnryX72 <negrettoenrico@gmail.com>
2023-10-30 18:40:49 +0100

    added stampaStudenti and still testing

--- src/App.java
+++ src/App.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class App {
     static Scanner tastiera = new Scanner(System.in);
 
-    static int nStudenti = 5;
+    static int nStudenti = 1;
     public static void main(String[] args) {
         String studenti[] = new String[nStudenti];
         int voti[] = new int[nStudenti];
 
         caricaStudenti(studenti);
+        stampaStudenti(studenti);
     }
 
     public static void caricaStudenti(String[] studenti){
         for(int i = 0; i < nStudenti; i++){
-            System.out.println("Inserisci il nome dello studnete " + (i+1) + ": ");
-            studenti[i] = tastiera.nextLine();
+            System.out.println("Inserisci il nome dello studente " + (i+1) + ": ");
+            studenti[i] = tastiera.nextLine().trim();
+        }
+    }
+
+    public static void stampaStudenti(String[] studenti){
+        for(int i = 0; i < nStudenti; i++){
+            System.out.println("Studente [" + (i+1) + "] = " + studenti[i]);
         }
     }
 }
